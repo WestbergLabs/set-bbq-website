e2764a7283158e9f39b5273e01063ac67aec5e16
4thehalibit <4thehalibit@gmail.com>
2026-09-14 12:53:25 -0500

    Bump build for customer Reply-To fix

--- js/site-version.js
+++ js/site-version.js
@@ -1,5 +1,5 @@
 (() => {
-  const SITE_VERSION = 'v0.1.8';
+  const SITE_VERSION = 'v0.1.9';
 
   document.addEventListener('DOMContentLoaded', () => {
     const footer = document.querySelector('.site-footer .container');
