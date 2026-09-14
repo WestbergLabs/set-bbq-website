6fe4a049190d320cc30e29b43daa7f75d6280ea0
4thehalibit <4thehalibit@gmail.com>
2026-09-14 11:44:37 -0500

    Bump test build version

--- js/site-version.js
+++ js/site-version.js
@@ -1,5 +1,5 @@
 (() => {
-  const SITE_VERSION = 'v0.1.3';
+  const SITE_VERSION = 'v0.1.4';
 
   document.addEventListener('DOMContentLoaded', () => {
     const footer = document.querySelector('.site-footer .container');
