4d7fbdc81125600b3f986bfd40d4da35f0c7c454
4thehalibit <4thehalibit@gmail.com>
2026-09-14 09:01:02 -0500

    Add EmailJS configuration placeholder

---
+++ js/emailjs-config.js
@@ -0,0 +1,9 @@
+// EmailJS configuration for the static GitHub Pages order form.
+// Replace the YOUR_* values after creating the EmailJS service and templates.
+window.SET_EMAIL_CONFIG = {
+  publicKey: 'YOUR_EMAILJS_PUBLIC_KEY',
+  serviceId: 'YOUR_EMAILJS_SERVICE_ID',
+  businessTemplateId: 'YOUR_EMAILJS_BUSINESS_TEMPLATE_ID',
+  customerTemplateId: 'YOUR_EMAILJS_CUSTOMER_TEMPLATE_ID',
+  businessEmail: 'northquarterscook@yahoo.com'
+};
