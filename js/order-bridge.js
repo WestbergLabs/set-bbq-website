abad6f242b96308063f0208bdbbb2c794066b130
4thehalibit <4thehalibit@gmail.com>
2026-09-14 09:02:10 -0500

    Bridge existing order data into EmailJS flow

---
+++ js/order-bridge.js
@@ -0,0 +1,4 @@
+// Bridge the existing order UI state/functions to the static EmailJS submission layer.
+window.orderState = orderState;
+window.buildOrderItems = buildOrderItems;
+window.calculateSubtotals = calculateSubtotals;
