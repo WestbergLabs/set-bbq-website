ee31c699ac5f6e1d8ab8e1d2d58df4e1367c1ff8
4thehalibit <4thehalibit@gmail.com>
2026-09-14 16:46:17 -0500

    Bump site build version

--- js/site-version.js
+++ js/site-version.js
@@ -1,5 +1,5 @@
 (() => {
-  const SITE_VERSION = 'v0.1.14';
+  const SITE_VERSION = 'v0.1.15';
 
   document.addEventListener('DOMContentLoaded', () => {
     const footer = document.querySelector('.site-footer .container');
