4b8bc70e149b3b379d534a1df8770587c4949705
4thehalibit <4thehalibit@gmail.com>
2026-09-15 11:20:34 -0500

    Move menu editor to Supabase database

--- js/site-version.js
+++ js/site-version.js
@@ -1,5 +1,5 @@
 (() => {
-  const SITE_VERSION = 'v0.1.27';
+  const SITE_VERSION = 'v0.1.28';
 
   document.addEventListener('DOMContentLoaded', () => {
     document.querySelectorAll('[data-site-version]').forEach((el) => {
