39992311346b9c81e2bf1d5cd37e716daed834b3
4thehalibit <4thehalibit@gmail.com>
2026-09-15 13:31:06 -0500

    Bump admin build to v0.1.35

--- js/site-version.js
+++ js/site-version.js
@@ -1,5 +1,5 @@
 (() => {
-  const SITE_VERSION = 'v0.1.34';
+  const SITE_VERSION = 'v0.1.35';
 
   document.addEventListener('DOMContentLoaded', () => {
     document.querySelectorAll('[data-site-version]').forEach((el) => {
