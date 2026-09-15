9d40eed30378460f7b289e54efb89e10711c9cd5
4thehalibit <4thehalibit@gmail.com>
2026-09-15 09:34:52 -0500

    Bump site version for mobile email

--- js/site-version.js
+++ js/site-version.js
@@ -1,5 +1,5 @@
 (() => {
-  const SITE_VERSION = 'v0.1.25';
+  const SITE_VERSION = 'v0.1.26';
 
   document.addEventListener('DOMContentLoaded', () => {
     document.querySelectorAll('[data-site-version]').forEach((el) => {
