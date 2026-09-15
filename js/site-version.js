a25b943832f818ff6f9d7dd825d198571dd33b24
4thehalibit <4thehalibit@gmail.com>
2026-09-15 09:24:03 -0500

    Bump site version for email alignment

--- js/site-version.js
+++ js/site-version.js
@@ -1,5 +1,5 @@
 (() => {
-  const SITE_VERSION = 'v0.1.24';
+  const SITE_VERSION = 'v0.1.25';
 
   document.addEventListener('DOMContentLoaded', () => {
     document.querySelectorAll('[data-site-version]').forEach((el) => {
