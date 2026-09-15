93e027c72485e40cc1b319f11a0e358d59776c71
4thehalibit <4thehalibit@gmail.com>
2026-09-15 16:25:00 -0500

    Bump site version for database-backed site content

--- js/site-version.js
+++ js/site-version.js
@@ -1,5 +1,5 @@
 (() => {
-  const SITE_VERSION = 'v0.1.45';
+  const SITE_VERSION = 'v0.1.46';
 
   document.addEventListener('DOMContentLoaded', () => {
     document.querySelectorAll('[data-site-version]').forEach((el) => {
