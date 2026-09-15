8ed7a8390907fc0803ad731264307e96f67882d6
4thehalibit <4thehalibit@gmail.com>
2026-09-15 12:13:44 -0500

    Bump site version to v0.1.33

--- js/site-version.js
+++ js/site-version.js
@@ -1,5 +1,5 @@
 (() => {
-  const SITE_VERSION = 'v0.1.32';
+  const SITE_VERSION = 'v0.1.33';
 
   document.addEventListener('DOMContentLoaded', () => {
     document.querySelectorAll('[data-site-version]').forEach((el) => {
